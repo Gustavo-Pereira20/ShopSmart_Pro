48ebf475bc66fd28bdf865ab4f1d16dd385d30cf
Gustavo Pereira <gustavo.h.pereira13@aluno.senai.br>
2026-02-13 14:56:30 -0300

    Ultimos detalhes

--- js/app.js
+++ js/app.js
@@ -7,20 +7,20 @@ function calcularTotal() {
     const v2 = parseFloat(input02.value);
 
     if (isNaN(v1) || isNaN(v2)) {
-
-        alert('Valores inválidos. DIgite um número.');
-
-    } else{
-
-        result.style.display = 'flex'
-
+        result.style.display = 'flex';
+        result.innerHTML = `
+            <h2>ERRO DE VALIDAÇÃO</h2>
+            <p>Valores inválidos. Digite um número.</p>
+        `;
+    } else {
+        result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-                <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
+                <li>Quantidade de itens: <span> ${v1} </span> </li>
+                <li>Preço individual: <span>${v2}</span> </li>       
                 <li>O valor total dos itens é ${v1} * ${v2}: <span>${v1 * v2}R$</span> </li>
             </ul>
-        `
+        `;
     }
 }
 
@@ -29,20 +29,20 @@ function desconto() {
     const v2 = parseFloat(input02.value);
 
     if (isNaN(v1) || isNaN(v2)) {
-
-        alert('Valores inválidos. DIgite um número.');
-
-    } else{
-
-        result.style.display = 'flex'
-
+        result.style.display = 'flex';
+        result.innerHTML = `
+            <h2>ERRO DE VALIDAÇÃO</h2>
+            <p>Valores inválidos. Digite um número.</p>
+        `;
+    } else {
+        result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-                <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
-                <li>O valor final de ${v1}R$ com ${v2}%  de desconto corresponde a: <span>${v1 - ((v1 * v2) / 100)}R$</span> </li>
+                <li>Valor do item: <span> ${v1} </span> </li>
+                <li>Porcentagem de desconto: <span>${v2}</span> </li>       
+                <li>O valor final de ${v1}R$ com ${v2}% de desconto corresponde a: <span>${v1 - ((v1 * v2) / 100)}R$</span> </li>
             </ul>
-        `
+        `;
     }
 }
 
@@ -51,20 +51,20 @@ function juros() {
     const v2 = parseFloat(input02.value);
 
     if (isNaN(v1) || isNaN(v2)) {
-
-        alert('Valores inválidos. DIgite um número.');
-
-    } else{
-
-        result.style.display = 'flex'
-
+        result.style.display = 'flex';
+        result.innerHTML = `
+            <h2>ERRO DE VALIDAÇÃO</h2>
+            <p>Valores inválidos. Digite um número.</p>
+        `;
+    } else {
+        result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-                <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
+                <li>Valor do item: <span> ${v1} </span> </li>
+                <li>Porcentagem de juros: <span>${v2}</span> </li>       
                 <li>O valor final de ${v1}R$ com acréscimo de ${v2}% de juros corresponde a: <span>${v1 + ((v1 * v2) / 100)}R$</span> </li>
             </ul>
-        `
+        `;
     }
 }
 
@@ -73,20 +73,20 @@ function comissao() {
     const v2 = parseFloat(input02.value);
 
     if (isNaN(v1) || isNaN(v2)) {
-
-        alert('Valores inválidos. DIgite um número.');
-
-    } else{
-
-        result.style.display = 'flex'
-
+        result.style.display = 'flex';
+        result.innerHTML = `
+            <h2>ERRO DE VALIDAÇÃO</h2>
+            <p>Valores inválidos. Digite um número.</p>
+        `;
+    } else {
+        result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-                <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
+                <li>Valor do item: <span> ${v1} </span> </li>
+                <li>Porcentagem de comissão: <span>${v2}</span> </li>       
                 <li>Em uma venda de ${v1}R$, a comissão de ${v2}% sobre o valor será de: <span>${v1 * (v2 / 100)}R$</span> </li>
             </ul>
-        `
+        `;
     }
 }
 
@@ -95,26 +95,26 @@ function lucro() {
     const v2 = parseFloat(input02.value);
 
     if (isNaN(v1) || isNaN(v2)) {
-
-        alert('Valores inválidos. DIgite um número.');
-
-    } else{
-
-        result.style.display = 'flex'
-
+        result.style.display = 'flex';
+        result.innerHTML = `
+            <h2>ERRO DE VALIDAÇÃO</h2>
+            <p>Valores inválidos. Digite um número.</p>
+        `;
+    } else {
+        result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor 01 digitado pelo usuário: <span> ${v1} </span> </li>
-                <li>Valor 02 digitado pelo usuário: <span>${v2}</span> </li>       
+                <li>Valor de compra: <span> ${v1} </span> </li>
+                <li>Valor de revenda: <span>${v2}</span> </li>       
                 <li>O lucro na compra de um produto de ${v1}R$ e na revenda por ${v2}R$ é de: <span>${v2 - v1}R$</span> </li>
             </ul>
-        `
+        `;
     }
 }
 
 function limpar() {
-    input01.value = ""
-    input02.value = ""
-    result.style.display = "none"
-    input01.focus()
+    input01.value = "";
+    input02.value = "";
+    result.style.display = "none";
+    input01.focus();
 }
