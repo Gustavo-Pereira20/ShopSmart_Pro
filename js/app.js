15095615642ff08f972b3642141348e25b4a42f5
Gustavo Pereira <gustavo.h.pereira13@aluno.senai.br>
2026-02-20 08:35:35 -0300

    Feito

--- js/app.js
+++ js/app.js
@@ -15,8 +15,8 @@ function calcularTotal() {
         result.innerHTML = `
             <ul>
                 <li>Quantidade de itens: <span> ${v1} </span> </li>
-                <li>Preço individual: <span> R$${v2} </span> </li>       
-                <li><strong>O valor total dos itens é ${v1} * ${v2}:</strong> <span> R$${(v1 * v2).toFixed(2)} </span> </li>
+                <li>Preço individual: <span> R$${(v2).toFixed(2)} </span> </li>       
+                <li><strong>O valor total dos itens é ${v1} * R$${(v2).toFixed(2)}:</strong> <span> R$${(v1 * v2).toFixed(2)} </span> </li>
             </ul>
         `;
     }
@@ -34,9 +34,9 @@ function desconto() {
         result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor do item: <span> R$${v1} </span> </li>
+                <li>Valor do item: <span> R$${(v1).toFixed(2)} </span> </li>
                 <li>Porcentagem de desconto: <span> ${v2}% </span> </li>       
-                <li><strong>O valor final de R$${v1} com ${v2}% de desconto corresponde a:</strong> <span> R$${(v1 - ((v1 * v2) / 100)).toFixed(2)} </span> </li>
+                <li><strong>O valor final de R$${(v1).toFixed(2)} com ${v2}% de desconto corresponde a:</strong> <span> R$${(v1 - ((v1 * v2) / 100)).toFixed(2)} </span> </li>
             </ul>
         `;
     }
@@ -54,9 +54,9 @@ function juros() {
         result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor do item: <span> R$${v1} </span> </li>
+                <li>Valor do item: <span> R$${(v1).toFixed(2)} </span> </li>
                 <li>Porcentagem de juros: <span> ${v2}% </span> </li>       
-                <li><strong>O valor final de R$${v1} com acréscimo de ${v2}% de juros corresponde a:</strong> <span>${(v1 + ((v1 * v2) / 100)).toFixed(2)}R$</span> </li>
+                <li><strong>O valor final de R$${(v1).toFixed(2)} com acréscimo de ${v2}% de juros corresponde a:</strong> <span>${(v1 + ((v1 * v2) / 100)).toFixed(2)}R$</span> </li>
             </ul>
         `;
     }
@@ -74,9 +74,9 @@ function comissao() {
         result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor do item: <span> R$${v1} </span> </li>
+                <li>Valor do item: <span> R$${(v1).toFixed(2)} </span> </li>
                 <li>Porcentagem de comissão: <span> ${v2}% </span> </li>       
-                <li><strong>Em uma venda de R$${v1}, a comissão de ${v2}% sobre o valor será de:</strong> <span>${(v1 * (v2 / 100)).toFixed(2)}R$</span> </li>
+                <li><strong>Em uma venda de R$${(v1).toFixed(2)}, a comissão de ${v2}% sobre o valor será de:</strong> <span>${(v1 * (v2 / 100)).toFixed(2)}R$</span> </li>
             </ul>
         `;
     }
@@ -94,9 +94,9 @@ function lucro() {
         result.style.display = 'flex';
         result.innerHTML = `
             <ul>
-                <li>Valor de venda: <span> R$${v1}</span> </li>
-                <li>Valor de compra: <span> R$${v2} </span> </li>   
-                <li><strong>O lucro na compra de um produto de R$${v2} e na revenda por R$${v1} é de:</strong> <span> R$${(v1 - v2).toFixed(2)} </span> </li>
+                <li>Valor de venda: <span> R$${(v1).toFixed(2)}</span> </li>
+                <li>Valor de compra: <span> R$${(v2).toFixed(2)} </span> </li>   
+                <li><strong>O lucro na compra de um produto de R$${(v2).toFixed(2)} e na revenda por R$${(v1).toFixed(2)} é de:</strong> <span> R$${(v1 - v2).toFixed(2)} </span> </li>
             </ul>
         `;
     }
